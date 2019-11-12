9aba96f5b43fb16054eb057f439a9954fe290917
Phil Austin <paustin@eos.ubc.ca>
2019-11-11 17:36:38 -0800

    checkpoint

--- e340lib/grade_quizzes.py
+++ e340lib/grade_quizzes.py
@@ -2,6 +2,9 @@ import pandas as pd
 import click
 import json
 from pathlib import Path
+import numpy as np
+from fuzzywuzzy import fuzz
+import pdb
 
 
 def find_closest(the_id, good_ids):
@@ -38,8 +41,42 @@ def main(filenames):
 
     with open(filenames.name, "r") as infile:
         file_dict = json.load(infile)
+    # pdb.set_trace()
+    grade_file = list(Path().glob(file_dict["files"]["gradebook"]))[0]
+    ind_file = list(Path().glob(file_dict["files"]["remark_ind"]))[0]
+    group_file = list(Path().glob(file_dict["files"]["remark_group"]))[0]
+    df_grades = pd.read_csv(grade_file)
+    df_ind = pd.read_excel(ind_file)
+    df_group = pd.read_excel(group_file)
+    print(df_grades.columns)
+    print(df_ind.columns)
+    print(df_group.columns)
+    student_ids = df_grades['Student Number'].to_numpy()
+    student_ids = [int(item) for item in student_ids]
+    group_ids = [
+        "STUDENT ID #1",
+        "STUDENT ID #2",
+        "STUDENT ID #3",
+        "STUDENT ID #4",
+        "Total Score",
+    ]
+    group_scores = df_group[group_ids].to_numpy()
+    nrows, ncols = group_scores.shape
+    id_list = []
+    group_list = []
+    for i in range(nrows):
+        row_ids = group_scores[i, :4]
+        score = group_scores[i, 4]
+        row_list = []
+        for item in row_ids:
+            the_id = int(item)
+            id_list.append(the_id)
+            row_list.append({"id": the_id, "score": score})
+        group_list.append(row_list)
 
-    print(file_dict)
+    print(id_list, group_list)
+    ind_scores = df_ind[["STUDENT ID", "Total Score"]].to_numpy()
+    nrows, ncols = ind_scores.shape
 
 
 main()
