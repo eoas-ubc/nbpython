f74c498e6245e14db1d3fa28734f224efb3cda15
Phil Austin <paustin@eos.ubc.ca>
2019-12-08 20:55:57 -0800

    checkpoint

--- sphinxlib/modify_notebooks.py
+++ sphinxlib/modify_notebooks.py
@@ -20,18 +20,40 @@ def print_files(file_glob):
     pp.pprint(good_files)
     return good_files
 
+@click.group()
+def main():
+    """
+    set of tools for manipulating notebooks
+    """
+    pass
 
-@click.command()
+
+
+@main.command()
 @click.argument("notebook_path", type=str, nargs=1)
-def main(notebook_path):
+def nbinit(notebook_path):
+    """
+    \b 
+    notebook_path: str
+       folder with py versions of notebooks
+    \b
+    set the formats to "ipynb,py:percent" and
+    the notebook_metadaa_filter to
+         "all,-language_info,-toc,-latex_envs"
+
+    example:
+      python $sphinxlib/modify_notebooks.py nbinit .
+    """
     py_files = Path(notebook_path).glob("*.py")
     format_string = r'{"jupytext":{"formats":"ipynb,py:percent"}}'
     text_string = (
         r'{"jupytext":{"notebook_metadata_filter": "all,-language_info,-toc,-latex_envs"}}'
     )
+    header_string = r'{"latex_metadata": {"chead": "center head","lhead": "left head"} }'
     for item in py_files:
         jupytext([str(item), "--update-metadata", format_string])
         jupytext([str(item), "--update-metadata", text_string])
+        jupytext([str(item), "--update-metadata", header_string])
         jupytext([str(item), "--sync"])
 
 
